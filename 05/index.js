3da012ea549d8102b94352cac05a05082c3f04cf
marvindev2022 <mavirolero@gmail.com>
2022-09-06 20:03:35 -0300

    Calcular distancia entre dois pontos

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,11 @@
+let x1 = 1;
+let y1 = 1;
+let x2 = 1;
+let y2 = 4;
 
+function distanciaEntreDoisPontos() {
+    let distancia = (x2 - x1) ** 2 + (y2 - y1) ** 2;
+    let result = Math.sqrt(distancia);
+    console.log(result);
+}
+distanciaEntreDoisPontos(x1, y1, x2, y2);
