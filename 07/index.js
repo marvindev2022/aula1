026b4c4c10a77ebd2e0a43174b23c2c7afd0e892
marvindev2022 <mavirolero@gmail.com>
2022-09-06 21:07:33 -0300

    Calculadora de  infecção do covid-19

---
+++ 07/index.js
@@ -0,0 +1,12 @@
+//$$ P = Po \* x ^\frac{t}{7} $$
+
+let po = 1000;
+let x = 4;
+let t = 7;
+
+function covid_19() {
+    let p = po * x ** (t / t);
+
+    return p;
+}
+console.log(covid_19(po, x, t));
